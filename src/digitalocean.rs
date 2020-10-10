addc20a854c0afc2aed20375e38fd28b86cf36be
frohman04 <frohman04@users.noreply.github.com>
2020-10-09 23:00:29 -0400

    Add getting domain record from DO

--- src/digitalocean.rs
+++ src/digitalocean.rs
@@ -11,10 +11,10 @@ impl DigitalOceanClient {
     }
 
     /// Check to see if a domain is controlled by this DigitalOcean account
-    pub fn domain_exists(&self, domain: String) -> Result<bool, reqwest::Error> {
+    pub fn get_domain(&self, domain: String) -> Result<Option<Domain>, reqwest::Error> {
         let mut url = "https://api.digitalocean.com/v2/domains".to_string();
         let mut exit = false;
-        let mut found = false;
+        let mut obj: Option<Domain> = None;
 
         while !exit {
             let resp = ClientBuilder::new()
@@ -25,20 +25,61 @@ impl DigitalOceanClient {
                 .send()?
                 .json::<DomainsResp>()?;
 
-            if resp.domains.iter().any(|d| d.name == domain) {
+            obj = resp.domains.into_iter().filter(|d| d.name == domain).next();
+            if obj.is_some() {
                 exit = true;
-                found = true;
             } else if resp.links.pages.is_some() && resp.links.pages.clone().unwrap().next.is_some()
             {
                 url = resp.links.pages.unwrap().next.unwrap();
                 url = url.replace("http://", "https://");
             } else {
                 exit = true;
-                found = false;
             }
         }
 
-        Ok(found)
+        Ok(obj)
+    }
+
+    /// Check to see if a domain is controlled by this DigitalOcean account
+    pub fn get_record(
+        &self,
+        domain: String,
+        record: String,
+        rtype: String,
+    ) -> Result<Option<DomainRecord>, reqwest::Error> {
+        let mut url = format!(
+            "https://api.digitalocean.com/v2/domains/{}/records?type={}",
+            domain, rtype
+        );
+        let mut exit = false;
+        let mut obj: Option<DomainRecord> = None;
+
+        while !exit {
+            let resp = ClientBuilder::new()
+                .build()
+                .unwrap()
+                .get(&url.clone())
+                .header("Authorization", format!("Bearer {}", self.token))
+                .send()?
+                .json::<DomainRecordsResp>()?;
+
+            obj = resp
+                .domain_records
+                .into_iter()
+                .filter(|r| r.name == record)
+                .next();
+            if obj.is_some() {
+                exit = true;
+            } else if resp.links.pages.is_some() && resp.links.pages.clone().unwrap().next.is_some()
+            {
+                url = resp.links.pages.unwrap().next.unwrap();
+                url = url.replace("http://", "https://");
+            } else {
+                exit = true;
+            }
+        }
+
+        Ok(obj)
     }
 }
 
@@ -72,7 +113,7 @@ struct DomainsResp {
 }
 
 #[derive(Deserialize, Debug)]
-struct Domain {
+pub struct Domain {
     /// The name of the domain itself.  This should follow the standard domain format of domain.TLD.
     /// For instance, example.com is a valid domain name.
     name: String,
@@ -84,4 +125,41 @@ struct Domain {
     /// However, this attribute can also be used to get information about the SOA record, which is
     /// created automatically and is not accessible as an individual record resource.
     zone_file: String,
+}
+
+// /v2/domains/[domain]/records
+
+#[derive(Deserialize, Debug)]
+struct DomainRecordsResp {
+    domain_records: Vec<DomainRecord>,
+    meta: Meta,
+    links: Links,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DomainRecord {
+    /// A unique identifier for each domain record.
+    id: u32,
+    /// The type of the DNS record. For example: A, CNAME, TXT, ...
+    #[serde(alias = "type")]
+    typ: String,
+    /// The host name, alias, or service being defined by the record.
+    name: String,
+    /// Variable data depending on record type. For example, the "data" value for an A record would
+    /// be the IPv4 address to which the domain will be mapped. For a CAA record, it would contain
+    /// the domain name of the CA being granted permission to issue certificates.
+    data: String,
+    /// The priority for SRV and MX records.
+    priority: Option<u16>,
+    /// The port for SRV records.
+    port: Option<u16>,
+    /// This value is the time to live for the record, in seconds. This defines the time frame that
+    /// clients can cache queried information before a refresh should be requested
+    ttl: u16,
+    /// The weight for SRV records.
+    weight: Option<u16>,
+    /// An unsigned integer between 0-255 used for CAA records.
+    flags: Option<u8>,
+    /// The parameter tag for CAA records. Valid values are "issue", "issuewild", or "iodef"
+    tag: Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,14 @@ mod ip_retriever;
 fn main() {
     let args = cli::Args::parse_args();
     let client = digitalocean::DigitalOceanClient::new(args.token);
-    print!("{:?}", client.domain_exists(args.domain))
+
+    client
+        .get_domain(args.domain.clone())
+        .expect("Error while contacting DigitalOcean")
+        .expect("Unable to find domain in account");
+    let record = client
+        .get_record(args.domain, args.record, args.rtype)
+        .expect("Error while contacting DigitalOcean")
+        .expect("Unable to find record for domain with desired type");
+    print!("{:?}", record)
 }
