a198df3260484b7d3c8855b26205891aa33043d2
frohman04 <frohman04@users.noreply.github.com>
2020-10-17 20:37:36 -0400

    Add tests for main

--- src/main.rs
+++ src/main.rs
@@ -109,3 +109,262 @@ impl std::fmt::Display for Error {
         write!(f, "{:?}", self)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::digitalocean::{DigitalOceanClient, Domain, DomainRecord, Error};
+    use crate::run;
+    use std::net::{IpAddr, Ipv4Addr};
+
+    #[test]
+    fn test_create_record() {
+        let id = 123;
+        let domain = "google.com".to_string();
+        let record_name = "main".to_string();
+        let rtype = "A".to_string();
+        let ip_addr: IpAddr = Ipv4Addr::new(8, 8, 8, 8).into();
+
+        let client = TestClientImpl {
+            id: id.clone(),
+            domain: domain.clone(),
+            record: record_name.clone(),
+            rtype: rtype.clone(),
+            ip_addr: ip_addr.clone(),
+            get_domain_is_ok: true,
+            get_domain_is_some: true,
+            get_record_is_ok: true,
+            get_record_is_some: false,
+            update_record_is_ok: false,
+            create_record_is_ok: true,
+        };
+
+        let record = run(
+            Box::new(client),
+            domain.clone(),
+            record_name.clone(),
+            rtype.clone(),
+            ip_addr.clone(),
+        );
+
+        assert_eq!(
+            record.unwrap(),
+            DomainRecord {
+                id,
+                typ: rtype,
+                name: record_name,
+                data: ip_addr.to_string(),
+                priority: None,
+                port: None,
+                ttl: 60,
+                weight: None,
+                flags: None,
+                tag: None
+            }
+        )
+    }
+
+    #[test]
+    fn test_update_record() {
+        let id = 123;
+        let domain = "google.com".to_string();
+        let record_name = "main".to_string();
+        let rtype = "A".to_string();
+        let ip_addr: IpAddr = Ipv4Addr::new(8, 8, 8, 8).into();
+        let new_ip_addr: IpAddr = Ipv4Addr::new(4, 4, 4, 4).into();
+
+        let client = TestClientImpl {
+            id: id.clone(),
+            domain: domain.clone(),
+            record: record_name.clone(),
+            rtype: rtype.clone(),
+            ip_addr: ip_addr.clone(),
+            get_domain_is_ok: true,
+            get_domain_is_some: true,
+            get_record_is_ok: true,
+            get_record_is_some: true,
+            update_record_is_ok: true,
+            create_record_is_ok: false,
+        };
+
+        let record = run(
+            Box::new(client),
+            domain.clone(),
+            record_name.clone(),
+            rtype.clone(),
+            new_ip_addr.clone(),
+        );
+
+        assert_eq!(
+            record.unwrap(),
+            DomainRecord {
+                id,
+                typ: rtype,
+                name: record_name,
+                data: new_ip_addr.to_string(),
+                priority: None,
+                port: None,
+                ttl: 60,
+                weight: None,
+                flags: None,
+                tag: None
+            }
+        )
+    }
+
+    #[test]
+    fn test_no_op() {
+        let id = 123;
+        let domain = "google.com".to_string();
+        let record_name = "main".to_string();
+        let rtype = "A".to_string();
+        let ip_addr: IpAddr = Ipv4Addr::new(8, 8, 8, 8).into();
+        let new_ip_addr: IpAddr = Ipv4Addr::new(8, 8, 8, 8).into();
+
+        let client = TestClientImpl {
+            id: id.clone(),
+            domain: domain.clone(),
+            record: record_name.clone(),
+            rtype: rtype.clone(),
+            ip_addr: ip_addr.clone(),
+            get_domain_is_ok: true,
+            get_domain_is_some: true,
+            get_record_is_ok: true,
+            get_record_is_some: true,
+            update_record_is_ok: false,
+            create_record_is_ok: false,
+        };
+
+        let record = run(
+            Box::new(client),
+            domain.clone(),
+            record_name.clone(),
+            rtype.clone(),
+            new_ip_addr.clone(),
+        );
+
+        assert_eq!(
+            record.unwrap(),
+            DomainRecord {
+                id,
+                typ: rtype,
+                name: record_name,
+                data: new_ip_addr.to_string(),
+                priority: None,
+                port: None,
+                ttl: 60,
+                weight: None,
+                flags: None,
+                tag: None
+            }
+        )
+    }
+
+    struct TestClientImpl {
+        id: u32,
+        domain: String,
+        record: String,
+        rtype: String,
+        ip_addr: IpAddr,
+        get_domain_is_ok: bool,
+        get_domain_is_some: bool,
+        get_record_is_ok: bool,
+        get_record_is_some: bool,
+        update_record_is_ok: bool,
+        create_record_is_ok: bool,
+    }
+
+    impl DigitalOceanClient for TestClientImpl {
+        fn get_domain(&self, _: &String) -> Result<Option<Domain>, Error> {
+            if self.get_domain_is_ok {
+                if self.get_domain_is_some {
+                    Ok(Some(Domain {
+                        name: self.domain.clone(),
+                        ttl: 60,
+                        zone_file: "foobar".to_string(),
+                    }))
+                } else {
+                    Ok(None)
+                }
+            } else {
+                Err(Error::Create("foo".to_string()))
+            }
+        }
+
+        fn get_record(
+            &self,
+            _: &String,
+            _: &String,
+            _: &String,
+        ) -> Result<Option<DomainRecord>, Error> {
+            if self.get_record_is_ok {
+                if self.get_record_is_some {
+                    Ok(Some(DomainRecord {
+                        id: self.id.clone(),
+                        typ: self.rtype.clone(),
+                        name: self.record.clone(),
+                        data: self.ip_addr.to_string(),
+                        priority: None,
+                        port: None,
+                        ttl: 60,
+                        weight: None,
+                        flags: None,
+                        tag: None,
+                    }))
+                } else {
+                    Ok(None)
+                }
+            } else {
+                Err(Error::Create("foo".to_string()))
+            }
+        }
+
+        fn update_record(
+            &self,
+            _: &String,
+            record: &DomainRecord,
+            value: &IpAddr,
+        ) -> Result<DomainRecord, Error> {
+            if self.update_record_is_ok {
+                Ok(DomainRecord {
+                    id: record.id.clone(),
+                    typ: record.typ.clone(),
+                    name: record.name.clone(),
+                    data: (*value).to_string(),
+                    priority: None,
+                    port: None,
+                    ttl: record.ttl.clone(),
+                    weight: None,
+                    flags: None,
+                    tag: None,
+                })
+            } else {
+                Err(Error::Update("foo".to_string()))
+            }
+        }
+
+        fn create_record(
+            &self,
+            _: &String,
+            record: &String,
+            rtype: &String,
+            value: &IpAddr,
+        ) -> Result<DomainRecord, Error> {
+            if self.create_record_is_ok {
+                Ok(DomainRecord {
+                    id: 123,
+                    typ: rtype.clone(),
+                    name: record.clone(),
+                    data: (*value).to_string(),
+                    priority: None,
+                    port: None,
+                    ttl: 60,
+                    weight: None,
+                    flags: None,
+                    tag: None,
+                })
+            } else {
+                Err(Error::Create("foo".to_string()))
+            }
+        }
+    }
+}
