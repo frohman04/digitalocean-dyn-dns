22ee1c92d18430ea5b6eb3f2e0f4fed1324de162
frohman04 <frohman04@users.noreply.github.com>
2024-04-28 14:05:15 -0400

    Add release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+set -x
+
+cargo test
+if [ -z ${OS+x} ]; then
+    BUILD=build
+else
+    # in Windows, so need to uze zigbuild for aarch64 compat
+    BUILD=zigbuild
+fi
+cargo ${BUILD} --release --target aarch64-unknown-linux-gnu
+scp target/aarch64-unknown-linux-gnu/release/digitalocean-dyn-dns gilneas:~
+ssh gilneas -- "chmod +x ~/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns /usr/local/bin/digitalocean-dyn-dns && sudo systemctl restart digitalocean-dyn-dns.service"
