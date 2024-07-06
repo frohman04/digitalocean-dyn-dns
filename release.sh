a5ba10fa56d8cc87fd74311b0f55ab51094890e7
frohman04 <frohman04@users.noreply.github.com>
2024-07-06 19:02:53 -0400

    Update release script

--- release.sh
+++ release.sh
@@ -11,5 +11,5 @@ else
     BUILD=zigbuild
 fi
 cargo ${BUILD} --release --target aarch64-unknown-linux-gnu
-scp digitalocean-dyn-dns.service target/aarch64-unknown-linux-gnu/release/digitalocean-dyn-dns gilneas:~
-ssh gilneas -- "chmod +x ~/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns /usr/local/bin/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns.service  /etc/systemd/system/digitalocean-dyn-dns.service; sudo systemctl daemon-reload && sudo systemctl restart digitalocean-dyn-dns.service"
+scp digitalocean-dyn-dns.service digitalocean-dyn-fw.service target/aarch64-unknown-linux-gnu/release/digitalocean-dyn-dns gilneas:~
+ssh gilneas -- "chmod +x ~/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns /usr/local/bin/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns.service  /etc/systemd/system/digitalocean-dyn-dns.service && sudo mv ~/digitalocean-dyn-fw.service  /etc/systemd/system/digitalocean-dyn-fw.service; sudo systemctl daemon-reload && sudo systemctl restart digitalocean-dyn-dns.service && sudo systemctl restart digitalocean-dyn-fw.service"
