e9582de6c5894367180b991feafc965d0006ee87
frohman04 <frohman04@users.noreply.github.com>
2025-01-10 19:00:07 -0500

    Fix clippy lint

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-cargo test
+cargo nextest run
 if [ -z ${OS+x} ]; then
     BUILD=build
 else
@@ -12,4 +12,4 @@ else
 fi
 cargo ${BUILD} --release --target aarch64-unknown-linux-gnu
 scp digitalocean-dyn-dns.service digitalocean-dyn-fw.service target/aarch64-unknown-linux-gnu/release/digitalocean-dyn-dns gilneas:~
-ssh gilneas -- "chmod +x ~/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns /usr/local/bin/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns.service  /etc/systemd/system/digitalocean-dyn-dns.service && sudo mv ~/digitalocean-dyn-fw.service  /etc/systemd/system/digitalocean-dyn-fw.service; sudo systemctl daemon-reload && sudo systemctl restart digitalocean-dyn-dns.service && sudo systemctl restart digitalocean-dyn-fw.service"
+ssh gilneas -- "chmod +x ~/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns /usr/local/bin/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns.service  /etc/systemd/system/digitalocean-dyn-dns.service && sudo mv ~/digitalocean-dyn-fw.service  /etc/systemd/system/digitalocean-dyn-fw.service; sudo systemctl daemon-reload && sudo systemctl restart digitalocean-dyn-dns.service"
