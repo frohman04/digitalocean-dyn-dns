2fc228cb76d05bc33c6522dd8a2901a2f4edb1f8
Chris Lieb <clieb@bitsighttech.com>
2025-04-18 21:27:04 -0400

    Fix broken Linux build on release

--- release.sh
+++ release.sh
@@ -5,11 +5,11 @@ set -x
 
 cargo nextest run
 if [ -z ${OS+x} ]; then
-    BUILD=build
+    # in Linux, use cross to build other platforms
+    CROSS_CONTAINER_UID=0 CROSS_CONTAINER_GID=0 cross build --release --target aarch64-unknown-linux-gnu
 else
     # in Windows, so need to uze zigbuild for aarch64 compat
-    BUILD=zigbuild
+    cargo zigbuild --release --target aarch64-unknown-linux-gnu
 fi
-cargo ${BUILD} --release --target aarch64-unknown-linux-gnu
 scp digitalocean-dyn-dns.service digitalocean-dyn-fw.service target/aarch64-unknown-linux-gnu/release/digitalocean-dyn-dns gilneas:~
 ssh gilneas -- "chmod +x ~/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns /usr/local/bin/digitalocean-dyn-dns && sudo mv ~/digitalocean-dyn-dns.service  /etc/systemd/system/digitalocean-dyn-dns.service && sudo mv ~/digitalocean-dyn-fw.service  /etc/systemd/system/digitalocean-dyn-fw.service; sudo systemctl daemon-reload && sudo systemctl restart digitalocean-dyn-dns.service"
